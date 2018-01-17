0ad8054fb9ac076603c9600f0a2612c9730125d2
Quentin Guenther <qguenther95@live.com>
2018-01-16 16:43:33 -0800

    Define an array called , which contains panda, alpaca, and boa.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Review Assignment: PHP Arrays & Functions</title>
+</head>
+<body>
+	<?php
+		$animals = array("panda", "alpaca", "boa");
+	?>
+</body>
+</html>
