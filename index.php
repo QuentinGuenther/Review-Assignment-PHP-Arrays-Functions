f389247f18bf500eab9d76f3243391ec9dc3ad4a
Quentin Guenther <qguenther95@live.com>
2018-01-16 19:00:22 -0800

    Define an associative array containing key-value pairs for various cupcake flavors. Use a loop to display the cupcake options as a set of checkboxes

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
     function sortAndPrint($arr) {
         asort($arr);
+        echo '<p>';
         foreach($arr as $item) {
             echo "$item ";
         }
+        echo '</p>';
+    }
+
+    function add($item, $arr) {
+        $item = strtolower($item);
+
+        if(!in_array($item, $arr)) {
+            echo "<p>adding $item...</p>";
+
+            array_push($arr, $item);
+        } else {
+            echo "<p>$item is already in the array.</p>";
+        }
+
+        return $arr;
     }
 ?>
 <!DOCTYPE html>
@@ -13,9 +29,21 @@
 </head>
 <body>
     <?php
-		$animals = array("panda", "alpaca", "boa");
-
+		$animals = array('panda', 'alpaca', 'boa');
 		sortAndPrint($animals);
+        $animals = add('Goat', $animals);
+        sortAndPrint($animals);
+
+        $cupcakeFlavors = array('grasshopper' => 'The Grasshopper',
+                                'maple' => 'Whiskey Maple Bacon',
+                                'carrot' => 'Carrot Walnut',
+                                'caramel' => 'Salted Caramel Cupcake',
+                                'velvet' => 'Red Velvet',
+                                'lemon' => 'Lemon Drop',
+                                'tiramisu' => 'Tiramisu');
+        foreach($cupcakeFlavors as $key => $value) {
+            echo '<input type="checkbox" name="flavors[]" value="'.$key.'"> '.$value.'<br>';
+        }
     ?>
 </body>
 </html>
